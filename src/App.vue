1ce4ea0a686d0c5bbbdaf796ccd179b2076899da
Iwryyy <l1uzc@foxmail.com>
2022-01-17 11:24:30 +0800

    day1 导入vant 完成了rem适配 以及简单封装了axios

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     1232132
     <router-view/>
+    <span class="iconfont icon-youjiantou"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <p class="title">asdkljsadlkj</p>
   </div>
 </template>
 
@@ -13,5 +20,7 @@ export default {
 </script>
 
 <style lang="less">
-
+  .title{
+    font-size: 30px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
 
 // // 导入less全局样式
 import '@/styles/index.less'
+
+// 按需引入vant
+import { Button } from 'vant'
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
